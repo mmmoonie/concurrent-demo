972378005fb8de1012d41dc09307ba062606dd71
moonie <wangchao_moonie@163.com>
2018-02-27 18:18:46 +0800

    init project

---
+++ src/main/java/xyz/supermoonie/App.java
@@ -0,0 +1,12 @@
+package xyz.supermoonie;
+
+/**
+ * @author moonie
+ * @date 2018/2/27
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
